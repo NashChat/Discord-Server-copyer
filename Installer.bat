9b5a9d9ae9f31081778ff95d1058c1945f30ba8a
Orya Mert Kansız <164668378+NashChat@users.noreply.github.com>
2024-07-08 14:57:53 +0300

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,10 @@
+@echo off
+pip install psutil
+pip install Presence
+pip install time
+pip install sys
+pip install discord
+pip install asyncio
+pip install colorama
+pip install fore
+pip install platform
